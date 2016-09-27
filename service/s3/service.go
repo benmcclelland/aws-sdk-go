46edadaf686c251480377af50ecd256596c4f493
Ben McClelland <ben.mcclelland@versity.com>
2016-09-26 19:12:27 -0700

    S3 V2 authentication
    work in progress
    
    seems to work against aws
    but having issues against some other third party servers
    
    config for 3rd party servers likely needs pathstyle:
    pathsyle := true
    config.S3ForcePathStyle = &pathstyle
    
    documentation for the authentication method is here:
    http://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html
    but is not super obvious for all cases

--- service/s3/service.go
+++ service/s3/service.go
@@ -7,8 +7,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws/client"
 	"github.com/aws/aws-sdk-go/aws/client/metadata"
 	"github.com/aws/aws-sdk-go/aws/request"
-	"github.com/aws/aws-sdk-go/aws/signer/v4"
+	//"github.com/aws/aws-sdk-go/aws/signer/v4"
 	"github.com/aws/aws-sdk-go/private/protocol/restxml"
+	"github.com/aws/aws-sdk-go/private/signer/s3v2"
 )
 
 // S3 is a client for Amazon S3.
@@ -58,7 +59,7 @@ func newClient(cfg aws.Config, handlers request.Handlers, endpoint, signingRegio
 	}
 
 	// Handlers
-	svc.Handlers.Sign.PushBackNamed(v4.SignRequestHandler)
+	svc.Handlers.Sign.PushBackNamed(s3v2.SignRequestHandler)
 	svc.Handlers.Build.PushBackNamed(restxml.BuildHandler)
 	svc.Handlers.Unmarshal.PushBackNamed(restxml.UnmarshalHandler)
 	svc.Handlers.UnmarshalMeta.PushBackNamed(restxml.UnmarshalMetaHandler)
